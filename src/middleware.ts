d1a86c82677d176fe975d9d1c3e2b36d7a42bf0e
FarouqJalabi <farouqjalabi@gmail.com>
2024-07-16 21:22:40 +0200

    middleware handling redirects error free

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,30 @@
-// ! Shuold handle login/register and signout
 import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export default withAuth({
-  callbacks: {
-    authorized: ({ req, token }) => {
-      if (req.url.includes("/admineditor")) {
-        return token?.email === "admin@hotmail.com";
-      }
-
-      if (req.url.includes("/login") && token != null) {
-        return false;
-      } else if (req.url.includes("/register") && token != null) {
-        return false;
-      } else if (req.url.includes("/signout") && token == null) {
-        return false;
-      }
-      return true;
-    },
+export default withAuth(
+  //
+  function middleware(req) {
+    const token = req.nextauth.token;
+    if (req.url.includes("/login") && token != null) {
+      return NextResponse.redirect(new URL("/signout", req.url));
+    } else if (req.url.includes("/register") && token != null) {
+      return NextResponse.redirect(new URL("/signout", req.url));
+    } else if (req.url.includes("/signout") && token == null) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
   },
-});
+  {
+    callbacks: {
+      authorized: ({ req, token }) => {
+        if (req.url.includes("/admineditor")) {
+          return token?.email === "admin@hotmail.com";
+        }
 
-// export const config = { matcher: ["/admineditor"] };
+        return true;
+      },
+    },
+  }
+);
 export const config = {
   matcher: ["/admineditor", "/signout", "/login", "/register"],
 };
