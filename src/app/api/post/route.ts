02624a33222213f7b90066e1cc679c50c8c15563
FarouqJalabi <farouqjalabi@gmail.com>
2024-07-16 21:22:36 +0200

    Admin able to post flowers, and flowerPreview

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,18 +1,53 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 import { NextRequest } from "next/server";
 import { revalidatePath } from "next/cache";
+import { getToken } from "next-auth/jwt";
 
 const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
+  const token = await getToken({ req });
   const body = await req.json();
-  const { formData, flowerData } = body;
-  console.log(formData, "formData");
-  await prisma.flowerPreviews.create({
-    data: { title: formData.title, underTitle: formData.underTitle },
-  });
-  revalidatePath("/");
 
-  // console.log(flowerData, "flowerData");
-  return new Response("OK");
+  if (token) {
+    const tokenValues = JSON.stringify(token, null, 2);
+    const userEmail = JSON.parse(tokenValues).user.email;
+
+    if (userEmail === "admin@hotmail.com") {
+      let { formData, flowersData } = body;
+      if (formData || flowersData) {
+        console.log(formData, "formData");
+        flowersData = flowersData.filter((flower: any) => {
+          flower.price = Number(flower.price);
+          flower.salePrice = Number(flower.salePrice);
+          delete flower.img;
+          return true;
+        });
+        console.log("filtered");
+        const flowerPreview_res = await prisma.flowerPreviews.create({
+          data: {
+            title: formData.title,
+            underTitle: formData.underTitle,
+            flowers: {
+              create: [...flowersData],
+            },
+          },
+          include: {
+            flowers: { select: { id: true } }, // Include all flowers in the returned object
+          },
+        });
+
+        console.log("sent");
+        const flower_id = flowerPreview_res.flowers.map((flower) => {
+          return flower.id;
+        });
+
+        console.log("Should return");
+        return new Response(JSON.stringify(flower_id));
+      }
+
+      revalidatePath("/");
+    }
+  }
+  return new Response("401");
 }
