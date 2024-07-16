9729ce632f3e5d5acf70afdd2486e847e8e34043
FarouqJalabi <farouqjalabi@gmail.com>
2024-07-16 21:22:40 +0200

    Flower page picture overlay bug

--- src/app/flower/[id]/page.tsx
+++ src/app/flower/[id]/page.tsx
@@ -22,7 +22,8 @@ export const metadata: Metadata = {
 
     images: [
       {
-        url: "https://eljnfbtxmeteozramfkt.supabase.co/storage/v1/object/public/flower_images/clksqth2f00019us4xrma6cbg.jpg",
+        url:
+          "https://eljnfbtxmeteozramfkt.supabase.co/storage/v1/object/public/flower_images/clksqth2f00019us4xrma6cbg.jpg",
         // "https://eljnfbtxmeteozramfkt.supabase.co/storage/v1/object/public/flower_images/" +
         // params.id +
         // "clksqth2f00019us4xrma6cbg" +
@@ -35,32 +36,6 @@ export const metadata: Metadata = {
     type: "website",
   },
 };
-// export async function generateMetadata({ params }: props): Promise<Metadata> {
-//   // const id = params.id
-//   return {
-//     title: "Flower Shop",
-//     description: "Mock flower shop",
-//     openGraph: {
-//       title: "Flower shop: Flower",
-//       url: "https://flower-shop-three.vercel.app/",
-//       siteName: "Flower Shop",
-
-//       images: [
-//         {
-//           url:
-//             "https://eljnfbtxmeteozramfkt.supabase.co/storage/v1/object/public/flower_images/" +
-//             // params.id +
-//             "clksqth2f00019us4xrma6cbg" +
-//             ".jpg",
-//           alt: "Flower image",
-//           width: 800,
-//           height: 600,
-//         },
-//       ],
-//       type: "website",
-//     },
-//   };
-// }
 
 export default async function Page({ params }: props) {
   const data = await getServerSession(options);
@@ -87,7 +62,7 @@ export default async function Page({ params }: props) {
   }
   return (
     <div className="flex sm:flex-row flex-col ">
-      <div className="relative w-80">
+      <div className="relative w-full sm:w-80">
         <Image
           src={
             "https://eljnfbtxmeteozramfkt.supabase.co/storage/v1/object/public/flower_images/" +
@@ -95,7 +70,7 @@ export default async function Page({ params }: props) {
             ".jpg"
           }
           alt={Info.alt}
-          className="flex-1 max-sm:w-full object-cover absolute "
+          className="flex-1 max-sm:w-full object-cover"
           priority
           width={800}
           height={600}
